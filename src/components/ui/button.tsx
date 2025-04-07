f9cff76fa7fffe58c9fc93ab1a233a72a43ecac3
Daniel Soliman <73758068+danielsoliman@users.noreply.github.com>
2025-04-07 12:11:09 -0400

    misc fixes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,6 +20,8 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        social:
+          "bg-blue-500 text-white shadow-xs hover:bg-blue-600 focus-visible:ring-blue-500/20 dark:focus-visible:ring-blue-400/40 dark:bg-blue-600",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
